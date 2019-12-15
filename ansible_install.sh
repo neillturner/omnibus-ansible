aa6a67ebca45c8352ebf32fde6f66e306ddbdc2d
juju4 <juju4@users.noreply.github.com>
2019-12-15 14:42:13 -0500

    Revert "add PATH update for /usr/local/bin"
    This reverts commit ca60adf1671a112feff246a4bd9ea0621e7aef5f.

--- ansible_install.sh
+++ ansible_install.sh
@@ -155,8 +155,6 @@ if [ ! "$(which ansible-playbook)" ]; then
   which ansible-playbook
   find / -iname ansible
   find / -iname ansible-playbook
-  cat /etc/environment
-  echo PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games" > /etc/environment
   set +x
   if [ -f /etc/centos-release ] || [ -f /etc/redhat-release ] || [ -f /etc/oracle-release ] || [ -f /etc/system-release ]; then
     # Fix for pycrypto pip / yum issue
